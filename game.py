50ad39eb5dd509aae606a9eb3d290931a3a880b7
Tiela Black-Law <tiela.rose@gmail.com>
2023-02-21 19:45:02 -0800

    Add greet player function

--- game.py
+++ game.py
@@ -4,3 +4,13 @@
 # computer will pick a random number between 1 and 100
 # user will guess a number; computer will say, "too low" or "too high"
 # when user guesses correctly, display number of guesses
+
+
+def greet_player():
+    print("Howdy, what's your name?")
+    name = input("(type in your name) ").title().rstrip()
+    print(f"{name}, I'm thinking of a number between 1 and 100.")
+    print("Try to guess my number.")
+
+
+greet_player()
