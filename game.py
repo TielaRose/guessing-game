b021fc2b49eb67f7c7322d58d1cc99b8eacf6b37
Tiela Black-Law <tiela.rose@gmail.com>
2023-02-21 20:15:04 -0800

    Fix name scope issue, add play_game function

--- game.py
+++ game.py
@@ -6,31 +6,37 @@ import random
 # user will guess a number; computer will say, "too low" or "too high"
 # when user guesses correctly, display number of guesses
 
-name = ""
 
-
-def greet_player():
+def get_name():
     print("Howdy, what's your name?")
     name = input("(type in your name) ").title().rstrip()
-    print(f"{name}, I'm thinking of a number between 1 and 100.")
-    print("Try to guess my number.")
+    return name
 
 
 def ask_for_guesses(name):
     random_number = random.choice(range(1, 101))
-    user_guess = 0
+    current_guess = 0
     num_tries = 0
-    while random_number != user_guess:
-        user_guess = int(input("Your guess? "))
+
+    while random_number != current_guess:
+        current_guess = int(input("Your guess? "))
         num_tries += 1
-        if user_guess < random_number:
+        if current_guess < random_number:
             print("Your guess is too low, try again!")
-        elif user_guess > random_number:
+        elif current_guess > random_number:
             print("Your guess is too high, try again!")
         else:
             break
     print(f"Well done, {name}! You found my number in {num_tries} tries!")
 
 
-greet_player()
-ask_for_guesses(name)
+def play_game():
+    name = get_name()
+
+    print(f"{name}, I'm thinking of a number between 1 and 100.")
+    print("Try to guess my number.")
+
+    ask_for_guesses(name)
+
+
+play_game()
