b9247a2da38c5f938d6ec12939317878742e1fa9
Tiela Black-Law <tiela.rose@gmail.com>
2023-02-21 19:36:36 -0800

    Initial commit

---
+++ game.py
@@ -0,0 +1,6 @@
+"""A number-guessing game."""
+
+# ask user for their name
+# computer will pick a random number between 1 and 100
+# user will guess a number; computer will say, "too low" or "too high"
+# when user guesses correctly, display number of guesses
